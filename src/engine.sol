2974dc6929ebd52a7b9834e3d9cde16cd3f8453b
himxa <himxa0x@gmail.com>
2026-01-18 15:50:18 +0100

    added comment where needed

--- src/engine.sol
+++ src/engine.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.19;
 
 import {StableToken} from "./StableToken.sol";
-
 /**
  * @title RaffileEngine
  * @author Himxa
@@ -38,16 +37,17 @@ contract RaffileEngine {
     event UserBuyToken(address indexed user, uint256 amount);
     event UserSellToken(address indexed user, uint256 amount);
     event UserBuyTickets(address indexed user, uint256 amount);
-    event userEnterRaffle(address indexed user, uint256 amount);
+    event UserEnterRaffle(address indexed user, uint256 amount);
     event RewardWinnerPicked(uint256 indexed roundId, address indexed winner);
     event RewardClaimed(address indexed winner, uint256 amount);
 
+    /*//////////////////////////////////////////////////////////////
+                                ENUMS
+    //////////////////////////////////////////////////////////////*/
     enum RaffleState {
         Open,
         Closed
     }
-
-    RaffleState currentState;
 
     /*//////////////////////////////////////////////////////////////
                           STATE VARIABLES
@@ -57,7 +57,8 @@ contract RaffileEngine {
     uint256 public raffleId;
     uint256 public entranceFee = 5e18;
     uint256 public maxTicketsPerRound = 10;
-    uint256 public currentRoundId;
+
+    RaffleState public currentState;
 
     /// @notice Ticket balance per user
     mapping(address => uint256) public ticketBalance;
@@ -68,8 +69,10 @@ contract RaffileEngine {
     /// @notice Total tickets sold per raffle round
     mapping(uint256 => uint256) public roundTotalTickets;
 
-    /// @notice Tracks whether a user has entered a specific round
+    /// @notice Tracks winner per round
     mapping(uint256 => address) public roundWinner;
+
+    /// @notice Tracks tickets used by a user per round
     mapping(uint256 => mapping(address => uint256)) public ticketsUsedPerRound;
 
     /*//////////////////////////////////////////////////////////////
@@ -86,6 +89,7 @@ contract RaffileEngine {
     //////////////////////////////////////////////////////////////*/
     constructor(address _stableTokenAddress) {
         stableToken = StableToken(_stableTokenAddress);
+        currentState = RaffleState.Open;
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -110,6 +114,7 @@ contract RaffileEngine {
         uint256 cost = tickets * entranceFee;
         ticketBalance[msg.sender] += tickets;
 
+        // Transfer StableToken from user to contract
         bool success = stableToken.transferFrom(msg.sender, address(this), cost);
         if (!success) {
             revert RaffileEngine__FailedToBuyTicket();
@@ -152,12 +157,16 @@ contract RaffileEngine {
         uint256 start = roundTotalTickets[raffleId] + 1;
         uint256 end = start + ticketsToUse - 1;
 
-        roundRanges[raffleId].push(TicketRange({start: start, end: end, owner: msg.sender}));
+        roundRanges[raffleId].push(TicketRange({
+            start: start,
+            end: end,
+            owner: msg.sender
+        }));
 
         // Update total tickets for the round
         roundTotalTickets[raffleId] = end;
 
-        emit userEnterRaffle(msg.sender, ticketsToUse);
+        emit UserEnterRaffle(msg.sender, ticketsToUse);
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -169,7 +178,7 @@ contract RaffileEngine {
      * @param random External random value
      * @return winner Address of winning participant
      */
-    function pickWinner(uint256 random) external returns (address) {
+    function pickWinner(uint256 random) external returns (address winner) {
         currentState = RaffleState.Closed;
         uint256 total = roundTotalTickets[raffleId];
         if (total == 0) {
@@ -208,24 +217,29 @@ contract RaffileEngine {
         currentState = RaffleState.Open;
     }
 
+    /**
+     * @notice Claims the reward for the winner of a round
+     * @param _roundId The round ID to claim reward from
+     */
     function claimRewardWon(uint256 _roundId) public {
-        if (roundWinner[_roundId] == address(0)) {
+        address winnerAddress = roundWinner[_roundId];
+        if (winnerAddress == address(0)) {
             revert RaffileEngine__RoundWinnerNotSet();
         }
 
-        if (roundWinner[_roundId] != msg.sender) {
+        if (winnerAddress != msg.sender) {
             revert RaffileEngine__YouAreNotTheWinner();
         }
 
         uint256 amountWon = roundTotalTickets[_roundId] * entranceFee;
 
-        bool success = stableToken.transfer(roundWinner[_roundId], amountWon);
-
+        // Transfer StableToken reward to winner
+        bool success = stableToken.transfer(winnerAddress, amountWon);
         if (!success) {
             revert RaffileEngine__failedToClaimReward();
         }
 
-        emit RewardClaimed(roundWinner[_roundId], amountWon);
+        emit RewardClaimed(winnerAddress, amountWon);
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -241,10 +255,10 @@ contract RaffileEngine {
         }
 
         bool success = stableToken.buyToken{value: msg.value}(msg.sender);
-
         if (!success) {
             revert RaffileEngine__FailedToBuyToken();
         }
+
         emit UserBuyToken(msg.sender, msg.value);
     }
 
@@ -262,16 +276,18 @@ contract RaffileEngine {
             revert RaffileEngine__InsufficientBalance();
         }
 
+        // Transfer StableToken from user to contract
         bool successT = stableToken.transferFrom(msg.sender, address(this), value);
-
         if (!successT) {
             revert RaffileEngine__FailedToSellToken();
         }
 
+        // Sell StableToken for ETH
         bool success = stableToken.sellToken(msg.sender, value);
         if (!success) {
             revert RaffileEngine__FailedToSellToken();
         }
+
         emit UserSellToken(msg.sender, value);
     }
 }
