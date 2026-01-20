a7fe9787e1bc2ff5ca585089180ad583da1a60e0
himxa <himxa0x@gmail.com>
2026-01-20 22:52:28 +0100

    setUp  script  for inter test

---
+++ script/deploy.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {StableToken} from "src/StableToken.sol";
+import {RaffileEngine} from "src/engine.sol";
+
+
+contract DeployEngine is Script {
+
+function run() public{
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+}
+
